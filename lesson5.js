e453eee130d81d7f3985d59f546d28834577baa3
Gustavo <92155053+Gunnar50@users.noreply.github.com>
2023-10-06 16:30:50 +0100

    Add files via upload

--- lesson5.js
+++ lesson5.js
@@ -40,3 +40,33 @@ const createBook = (title, author, noPages) => {
 };
 const harryPotter = createBook("Harry Potter", "JK Rowling", 200);
 console.log(harryPotter.details());
+// Use Rest on an array
+const [red, ...restOfColours] = colours;
+
+// Use Rest on an object
+const { title: bookTitle, ...restOfBook } = harryPotter;
+console.log(bookTitle);
+
+// Use Spread on an array
+// using array destruction to create the 2 arrays.
+const [arr1, arr2] = [
+	[1, 2, 3],
+	[4, 5, 6],
+];
+const addArr = [...arr1, ...arr2];
+
+// Use Spread on an Object
+harryPotterWithYear = { ...harryPotter, year: 1997 };
+console.log(harryPotterWithYear);
+
+// Create a class (any concept you like)
+class Furniture {
+	constructor(type, material) {
+		this.type = type;
+		this.material = material;
+	}
+
+	getDetails() {
+		return `${this.type} of the material ${this.material}`;
+	}
+}
